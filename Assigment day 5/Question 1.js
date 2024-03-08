326749b274d01f0c6ebc0d9bea3fc1fc92151378
Rohitkumarj07 <154002804+Rohitkumarj07@users.noreply.github.com>
2024-03-08 21:14:11 +0530

    Add files via upload

---
+++ Assigment day 5/Question 1.js
@@ -0,0 +1,31 @@
+//Resume data in JSON format
+const resume = {
+    "name": "Roney",
+    "contact": {
+      "email": "roney200142@gmail.com",
+      "phone": "+919500780653",
+      "address": "no,9 captain lakshmi segal street,varatharajanagar,periyakuppam,tiruvallur,thiruvallur 6602001"
+    },
+    "summary": " I’m a Final-year Computer Science  at Vels University, Chennai, I’ve always been passionate about cybersecurity and knowledge seeker and hard worker and im trying to make this place in software industry i will surely be a good software engineer for the company as well as to the society",
+    
+    "education": [
+      {
+        "degree": "B.Tech in Computer Science",
+        "institution": "VELS  UNIVERSITY",
+        "location": "chennai, Tamil nadu",
+      },
+    ],
+    "skills": [
+      "JavaScript",
+      "Node.js",
+      "React.js",
+      "HTML5",
+      "Python",
+      "Java",
+      "Git",
+    ],
+    "languages": ["Tamil","English","Hindi"]
+  };
+  
+  console.log(resume);
+  
